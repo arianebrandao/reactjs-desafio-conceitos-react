8a44ab05c312248528bc1a1067e731081aee23df
Ariane Brandao <ariane_mad@hotmail.com>
2021-07-13 22:30:10 -0300

    Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -14,16 +14,38 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  useEffect(() => {
+
+  }, [tasks])
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    //newTask = [...tasks, data];
+    if(newTaskTitle){
+      setTasks([...tasks, {
+        id: Date.now() + Math.random(), //https://stackoverflow.com/questions/8012002/create-a-unique-number-with-javascript-time
+        title: newTaskTitle,
+        isComplete: false
+      }]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task);
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    //if(window.confirm("Are you sure you want delete this task?")){
+      setTasks(tasks.filter((task)=>(task.id !== id))); //https://tyindpgood.medium.com/add-delete-item-from-state-array-in-react-hook-9d7f73d66d80
+    //}
+    console.log(tasks);
   }
 
   return (
